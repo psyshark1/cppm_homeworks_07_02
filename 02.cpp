0dcd8f9a2447fece1727f8a162f2dbe964b52c4f
Andrey <andrey.zalozhenkov@pikta.ru>
2023-07-19 15:49:55 +0400

    Добавьте файлы проекта.

---
+++ 02.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+#define SUB(a, b) ((a) - (b))
+
+int main(int argc, char** argv)
+{
+    std::system("chcp 1251");
+
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+
+    std::system("pause");
+    return 0;
+}
